21e1d4458012da9e8d4459313457600eacba2a12
MatteoPassalent <pass2102@gmail.com>
2023-07-12 11:04:26 -0400

    Initial assignment 4

---
+++ pass7410_a04/src/cp213/PopularityTree.java
@@ -0,0 +1,142 @@
+package cp213;
+
+import java.util.ArrayList;
+
+/**
+ * Implements a Popularity Tree. Extends BST.
+ *
+ * @author your name here
+ * @author David Brown
+ * @version 2023-06-04
+ */
+public class PopularityTree<T extends Comparable<T>> extends BST<T> {
+
+    /**
+     * Auxiliary method for valid. May force node rotation if the retrieval count of
+     * the located node item is incremented.
+     *
+     * @param node The node to examine for key.
+     * @param key  The item to search for. Count is updated to count in matching
+     *             node item if key is found.
+     * @return The updated node.
+     */
+    private TreeNode<T> retrieveAux(TreeNode<T> node, final CountedItem<T> key) {
+
+        // your code here
+
+        return null;
+    }
+
+    /**
+     * Performs a left rotation around node.
+     *
+     * @param parent The subtree to rotate.
+     * @return The new root of the subtree.
+     */
+    private TreeNode<T> rotateLeft(final TreeNode<T> parent) {
+
+        // your code here
+        TreeNode<T> pivot = parent.getRight();
+        parent.setRight(pivot.getRight());
+        pivot.setLeft(parent);
+        return pivot;
+    }
+
+    /**
+     * Performs a right rotation around {@code node}.
+     *
+     * @param parent The subtree to rotate.
+     * @return The new root of the subtree.
+     */
+    private TreeNode<T> rotateRight(final TreeNode<T> parent) {
+
+        // your code here
+        TreeNode<T> pivot = parent.getLeft();
+        parent.setLeft(pivot.getLeft());
+        pivot.setRight(parent);
+
+        return pivot;
+    }
+
+    /**
+     * Replaces BST insertAux - does not increment count on repeated insertion.
+     * Counts are incremented only on retrieve.
+     */
+    @Override
+    protected TreeNode<T> insertAux(TreeNode<T> node, final CountedItem<T> data) {
+
+        // your code here
+        if (node == null) {
+            // Base case - add a new node containing the data.
+            node = new TreeNode<T>(data);
+            this.size++;
+        } else {
+            // Compare the node data against the insert data.
+            final int result = node.getdata().compareTo(data);
+
+            if (result > 0) {
+                // General case - check the left subtree.
+                node.setLeft(this.insertAux(node.getLeft(), data));
+            } else if (result < 0) {
+                // General case - check the right subtree.
+                node.setRight(this.insertAux(node.getRight(), data));
+            } else {
+                // Base case - data is already in the tree, increment its count
+            }
+        }
+        node.updateHeight();
+        return node;
+
+    }
+
+    /**
+     * Auxiliary method for valid. Determines if a subtree based on node is a valid
+     * subtree. An Popularity Tree must meet the BST validation conditions, and
+     * additionally the counts of any node data must be greater than or equal to the
+     * counts of its children.
+     *
+     * @param node The root of the subtree to test for validity.
+     * @return true if the subtree base on node is valid, false otherwise.
+     */
+    @Override
+    protected boolean isValidAux(final TreeNode<T> node, TreeNode<T> minNode, TreeNode<T> maxNode) {
+
+        // your code here
+
+        return false;
+    }
+
+    /**
+     * Determines whether two PopularityTrees are identical.
+     *
+     * @param target The PopularityTree to compare this PopularityTree against.
+     * @return true if this PopularityTree and target contain nodes that match in
+     *         position, item, count, and height, false otherwise.
+     */
+    public boolean equals(final PopularityTree<T> target) {
+        return super.equals(target);
+    }
+
+    /**
+     * Very similar to the BST retrieve, but increments the data count here instead
+     * of in the insertion.
+     *
+     * @param key The key to search for.
+     */
+    @Override
+    public CountedItem<T> retrieve(CountedItem<T> key) {
+
+        // your code here
+        // def wrong
+        ArrayList<CountedItem<T>> list = this.inOrder();
+        for (int i = 0; i <= list.size(); i++) {
+            if (list.get(i) == key) {
+                list.get(i).incrementCount();
+                return list.get(i);
+            }
+        }
+
+        return null;
+    }
+
+}
